1794f9ac9aab797948bf24b3983ab5a91aa6ffe8
boookappa <159744966+boookappa@users.noreply.github.com>
2026-07-13 13:06:57 +0900

    Update fmt.Println message from 'Hello' to 'Goodbye'

--- china_app.py
+++ china_app.py
@@ -217,7 +217,7 @@ else:
                 st.info(f"フォルダ【{selected_test_folder}】にはまだデータがないぞ。")
             else:
                 # シャッフルと表示処理
-               shuffle_session_key = f"list_shuffled_{selected_test_folder}"
+                shuffle_session_key = f"list_shuffled_{selected_test_folder}"
                 # リセット用のキーをsession_stateで管理する
                 expander_reset_key = f"expander_reset_{selected_test_folder}"
                 if expander_reset_key not in st.session_state:
